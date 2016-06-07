9c5b0a8e016fe872b473b1aa12b9b97c2edd3049
Nelson Jeppesen <nelson@simplymeasured.com>
2016-06-07 13:55:11 -0700

    Add awscli to shared cask

--- Casks/my-station-shared.rb
+++ Casks/my-station-shared.rb
@@ -1,11 +1,12 @@
 cask 'my-station-shared' do
-  version '1.1'
+  version '1.2'
   url 'https://raw.githubusercontent.com/Jeppesen-io/homebrew-stations/master/Casks/my-station-shared.rb'
   sha256 :no_check
   container type: :naked
   stage_only true
 
   # Common brews
+  depends_on formula: 'awscli'
   depends_on formula: 'curl'
   depends_on formula: 'ipcalc'
   depends_on formula: 'jq'
