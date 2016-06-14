fa72d75023c95a6dbf31dbe806569bdc9c7226e5
Nelson Jeppesen <nelson@simplymeasured.com>
2016-06-14 15:03:31 -0700

    Add macos formula and bump. Remove cruft

--- Casks/my-station-shared.rb
+++ Casks/my-station-shared.rb
@@ -1,5 +1,5 @@
 cask 'my-station-shared' do
-  version '1.5'
+  version '1.6'
   url 'https://raw.githubusercontent.com/Jeppesen-io/homebrew-stations/master/Casks/my-station-shared.rb'
   sha256 :no_check
   container type: :naked
@@ -22,8 +22,10 @@ cask 'my-station-shared' do
   depends_on cask: 'spotify'
   depends_on cask: 'visual-studio-code'
 
-  # My brew/cask wrappers
-  depends_on formula: 'jeppesen-io/stations/my-osx'
+  # My formulas
+  depends_on formula: 'jeppesen-io/macos/my-macos'
+
+  # My casks
   depends_on cask:    'jeppesen-io/iterm/my-iterm'
   depends_on cask:    'jeppesen-io/git/my-git'
   depends_on cask:    'jeppesen-io/neovim/my-neovim'
