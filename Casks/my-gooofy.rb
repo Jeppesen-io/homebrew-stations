4ceacab1b278ee7ef2ca055f67a1acb22928a21a
Nelson Jeppesen <nelson@simplymeasured.com>
2016-07-06 16:57:23 -0700

    Pull gooofy to it's own forumlua

---
+++ Casks/my-gooofy.rb
@@ -0,0 +1,21 @@
+cask 'my-goofy' do
+  version '1.0'
+  url 'https://raw.githubusercontent.com/Jeppesen-io/homebrew-stations/master/Casks/my-goofy.rb'
+  sha256 :no_check
+  container type: :naked
+  stage_only true
+
+  depends_on cask: 'goofy'
+
+  postflight do
+
+    # Enable spellcheck
+    `defaults write cc.buechele.Goofy WebContinuousSpellCheckingEnabled -bool true`
+
+    # Disable auto-correct
+    `defaults write cc.buechele.Goofy WebContinuousSpellCheckingEnabled -bool false`
+
+    # Add status bar
+    `defaults write cc.buechele.Goofy showStatusItem -bool true`
+
+end
--- Casks/my-station-shared.rb
+++ Casks/my-station-shared.rb
@@ -1,5 +1,5 @@
 cask 'my-station-shared' do
-  version '1.8'
+  version '1.9'
   url 'https://raw.githubusercontent.com/Jeppesen-io/homebrew-stations/master/Casks/my-station-shared.rb'
   sha256 :no_check
   container type: :naked
@@ -22,7 +22,6 @@ cask 'my-station-shared' do
   depends_on cask: 'spotify'
   depends_on cask: 'visual-studio-code'
   depends_on cask: 'yakyak'
-  depends_on cask: 'goofy'
 
   # My formulas
   depends_on formula: 'jeppesen-io/macos/my-macos'
@@ -30,6 +29,7 @@ cask 'my-station-shared' do
   # My casks
   depends_on cask:    'jeppesen-io/iterm/my-iterm'
   depends_on cask:    'jeppesen-io/git/my-git'
+  depends_on cask:    'jeppesen-io/stations/my-goofy'
   depends_on cask:    'jeppesen-io/neovim/my-neovim'
   depends_on cask:    'jeppesen-io/bash/my-bash'
   depends_on cask:    'jeppesen-io/hammerspoon/my-hammerspoon'
