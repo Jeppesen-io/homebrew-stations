cbfde63bace0b8215c77374e054407af6302bcb7
Nelson <nelson@nelson-rmbp.corp.simplymeasured.com>
2016-11-11 15:28:28 -0800

    Fix chrome canary and pip

--- Casks/my-station-work.rb
+++ Casks/my-station-work.rb
@@ -1,5 +1,5 @@
 cask 'my-station-work' do
-  version '1.3'
+  version '1.4'
   url 'https://raw.githubusercontent.com/Jeppesen-io/homebrew-stations/master/Casks/my-station-work.rb'
   sha256 :no_check
   container type: :naked
@@ -10,13 +10,15 @@ cask 'my-station-work' do
   depends_on cask: 'vagrant'
 
   # Can use a different browser for personal work
-  depends_on cask: 'google-chrome-canary'
+  depends_on cask: 'homebrew/versions/google-chrome-canary'
 
   # Sometimes it's helpful to test with a different browser
   depends_on cask: 'firefox'
 
   # Stuff I use all on computers
   depends_on cask: 'jeppesen-io/stations/my-station-shared'
+
+  depends_on fomrula: 'python'
 
   postflight do
 
